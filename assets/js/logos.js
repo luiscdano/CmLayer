38acedb5d60b414ed0ab6b1615bfa65eff83f4d7
Luis Emilio Cedano <luiscdano@gmail.com>
2025-11-13 19:04:26 -0400

    Corrige rutas de logos flotantes para evitar sprites grises

--- assets/js/logos.js
+++ assets/js/logos.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   logos.forEach((logo, i) => {
     const img = document.createElement("img");
-    img.src = `/assets/img/${logo}`;
+    img.src = `assets/img/${logo}`;
     img.classList.add("floating-logo");
     img.style.top = `${Math.random() * 80 + 10}%`;
     img.style.left = `${Math.random() * 80 + 10}%`;
