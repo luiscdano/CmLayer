ca4a18998cd7fdf68b5d7c49c7184a9688202359
Luis Emilio Cedano <luiscdano@gmail.com>
2025-11-15 23:03:11 -0400

    Fix dynamic header/footer path resolution

--- assets/js/include.js
+++ assets/js/include.js
@@ -1,12 +1,18 @@
-// Insert header
-fetch("/header.html")
+// Detecta si estamos dentro de una carpeta
+const basePath = window.location.pathname.includes("/") &&
+                 window.location.pathname.split("/").length > 2
+                 ? "../"
+                 : "./";
+
+// Insertar Header
+fetch(basePath + "header.html")
   .then(res => res.text())
   .then(html => {
     document.getElementById("header-container").innerHTML = html;
   });
 
-// Insert footer
-fetch("/footer.html")
+// Insertar Footer
+fetch(basePath + "footer.html")
   .then(res => res.text())
   .then(html => {
     document.getElementById("footer-container").innerHTML = html;
